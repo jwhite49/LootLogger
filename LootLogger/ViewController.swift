1714688ebb00f58e6ef60680ecb5beb30fe56815
jordan white <jwhite49@jacksonville.edu>
2025-10-21 15:35:49 -0400

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Jordan White on 10/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
