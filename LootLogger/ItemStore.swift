e2e27295fa47dd1111b0722b5b37281b3149d282
jordan white <jwhite49@jacksonville.edu>
2025-11-13 16:12:08 -0500

    chapter 13 bronze and app

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -10,6 +10,12 @@ import UIKit
 class ItemStore {
 
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
 
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -40,6 +46,32 @@ class ItemStore {
         allItems.insert(movedItem, at: toIndex)
     }
     
+    @objc func saveChanges() throws {
+        print("Saving items to: \(itemArchiveURL)")
+
+        let encoder = PropertyListEncoder()
+        let data = try encoder.encode(allItems)
+        try data.write(to: itemArchiveURL, options: [.atomic])
+
+        print("Items are saved")
+    }
+
+    
+    init() {
+        do {
+                let data = try Data(contentsOf: itemArchiveURL)
+                let unarchiver = PropertyListDecoder()
+                let items = try unarchiver.decode([Item].self, from: data)
+                allItems = items
+            } catch {
+                print("Error reading in saved items: \(error)")
+            }
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
+    }
 
 }
 
